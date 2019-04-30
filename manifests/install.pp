09511673ee6a335ba4ade2286fa220dffaff95c8
tolkiger <33635964+tolkiger@users.noreply.github.com>
2019-04-30 17:14:53 -0500

    Changed yum package python-dev name
    Changed yum package python-dev name to python-devel since python-dev does not exist:
    
    No package python-dev available.
    Error: Nothing to do

--- manifests/install.pp
+++ manifests/install.pp
@@ -69,7 +69,7 @@ class python::install {
       }
 
       if $pythondev {
-        package { 'python-dev':
+        package { 'python-devel':
           ensure => $dev_ensure,
           name   => $pythondev,
         }
@@ -90,7 +90,7 @@ class python::install {
           Package <| title == 'virtualenv' |> {
             name     => 'virtualenv',
             provider => 'pip',
-            require  => Package['python-dev'],
+            require  => Package['python-devel'],
           }
         } else {
           Package <| title == 'virtualenv' |> {
@@ -211,7 +211,7 @@ class python::install {
             }
           }
           if $pythondev {
-            package { 'python-dev':
+            package { 'python-devel':
               ensure   => $dev_ensure,
               name     => $pythondev,
               alias    => $pythondev,
@@ -226,7 +226,7 @@ class python::install {
             require => Package['python'],
           }
           if $pythondev {
-            package { 'python-dev':
+            package { 'python-devel':
               ensure => $dev_ensure,
               name   => $pythondev,
               alias  => $pythondev,
